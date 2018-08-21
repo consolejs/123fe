6d2edf4745ca99a2d4fdfb646e16c238c2a54f74
Likun <likun205743@sogou-inc.com>
2018-08-21 17:32:23 +0800

    77777

--- autopush.sh
+++ autopush.sh
@@ -35,7 +35,7 @@ function autoPush()
 		echo  "🌻 输入commit内容🌻 :"
 		read commit_msg
         echo  "--------------------->"
-        echo  "🐙 提交日志中...🐙 : $commit_msg:"
+        echo  "🐙 提交日志中..."
         echo  "----------------"
 		# 判断是否commit成功
 		var=$(git commit -m "$commit_msg" 2>&1)
